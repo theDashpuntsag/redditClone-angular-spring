a0b62a29c589d3a888faf49d60a692b0b0e332b4
theDashpuntsag <theDashpuntsag@gmail.com>
2023-03-05 17:19:32 +0800

    .yml file added

--- reddit-clone/src/main/java/com/spring/tutorial/redditclone/RedditCloneApplication.java
+++ reddit-clone/src/main/java/com/spring/tutorial/redditclone/RedditCloneApplication.java
@@ -1,15 +1,68 @@
 package com.spring.tutorial.redditclone;
 
+import com.spring.tutorial.redditclone.config.CRLFLogConverter;
+import io.micrometer.common.util.StringUtils;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableAsync
 public class RedditCloneApplication {
+	private static final Logger log = LoggerFactory.getLogger(RedditCloneApplication.class);
+
+	public RedditCloneApplication(Environment env) {
+		this.env = env;
+	}
+
+	private final Environment env;
 
 	public static void main(String[] args) {
-		SpringApplication.run(RedditCloneApplication.class, args);
+		SpringApplication app = new SpringApplication(RedditCloneApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logApplicationStartup(env);
+	}
+
+	private static void logApplicationStartup(Environment env) {
+		String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+		String serverPort = env.getProperty("server.port");
+		String contextPath = Optional
+				.ofNullable(env.getProperty("server.servlet.context-path"))
+				.filter(StringUtils::isNotBlank)
+				.orElse("/");
+		String hostAddress = "localhost";
+		try {
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			log.warn("The host name could not be determined, using `localhost` as fallback");
+		}
+		log.info(
+				CRLFLogConverter.CRLF_SAFE_MARKER,
+				"\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\t{}://localhost:{}{}\n\t" +
+						"External: \t{}://{}:{}{}\n\t" +
+						"Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				protocol,
+				serverPort,
+				contextPath,
+				protocol,
+				hostAddress,
+				serverPort,
+				contextPath,
+				env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
+		);
 	}
 
 }
